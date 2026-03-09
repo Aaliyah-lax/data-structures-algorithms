160a111e77351f3ffddbd9a8eb3d09bf29e0b32b
Aaliyah-lax <aaliyah.laxa@gmail.com>
2026-03-08 18:37:50 -0700

    Add files via upload

---
+++ HashProject.java
@@ -0,0 +1,19 @@
+
+public class HashProject
+{
+
+    public static void main(String[] args)
+    {
+        LinearProbingHashST<String, Integer> st = new LinearProbingHashST<>();
+
+        String str = "Aaliyah";// YOur Name
+        for (int i = 0; i < str.length(); i++)
+        {
+            String key = str.substring(i, i + 1);
+            st.put(key, i);
+            st.print();
+
+        }
+
+    }
+}
